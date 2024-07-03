71c5ca7586b0cd8d5a22a5fa55d8c840e7b7f27d
Ramy Tanios <ramy.tanios@gmail.com>
2024-07-03 22:45:36 +0200

    progress reporter example

--- examples/src/main/scala/ProgressReporter.scala
+++ examples/src/main/scala/ProgressReporter.scala
@@ -1,1 +1,24 @@
+import cats.effect.IO
+import cats.effect.IOApp
+import scala.concurrent.duration.*
+import tldev.core.utils.ProgressReporter
 
+object ProgressReporterMain extends IOApp.Simple:
+
+  override def run: IO[Unit] =
+    cats.effect.std.Queue
+      .unbounded[IO, String]
+      .flatMap: logQueue =>
+        ProgressReporter
+          .toQueue[IO]("my_label", 1.seconds, logQueue)
+          .use: reporter =>
+            fs2.Stream
+              .fixedDelay[IO](3.seconds)
+              .zipWithIndex
+              .map((_, idx) => idx.toDouble)
+              .evalTap(reporter.setProgress(_))
+              .takeWhile(_ <= 100)
+              .concurrently:
+                fs2.Stream.fromQueueUnterminated(logQueue).printlns
+              .compile
+              .drain
