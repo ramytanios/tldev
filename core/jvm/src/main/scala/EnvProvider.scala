cb16a86ee1f7274ef53abec680cfbb85594ffd55
Ramy Tanios <ramy.tanios@gmail.com>
2025-01-13 10:00:07 +0100

    minor formatting

--- core/jvm/src/main/scala/EnvProvider.scala
+++ core/jvm/src/main/scala/EnvProvider.scala
@@ -14,7 +14,7 @@ import EnvProvider.*
 trait EnvProvider[F[_]]:
 
   /** access an environment variable */
-  def get[V](name: String)(using Parser[V]): F[Either[Error, V]]
+  def get[V: Parser](name: String): F[Either[Error, V]]
 
 object EnvProvider:
 
@@ -35,38 +35,34 @@ object EnvProvider:
 
     given Parser[Int] with
       override def parse(env: String): Either[Error, Int] =
-        Either.catchOnly[NumberFormatException](env.toInt).leftMap(t =>
+        Either.catchOnly[NumberFormatException](env.toInt).leftMap: t =>
           Error.ParsingError(s"failed to parse $env to `Int`: $t")
-        )
 
     given Parser[Long] with
       override def parse(env: String): Either[Error, Long] =
-        Either.catchOnly[NumberFormatException](env.toLong).leftMap(t =>
+        Either.catchOnly[NumberFormatException](env.toLong).leftMap: t =>
           Error.ParsingError(s"failed to parse $env to `Long`: $t")
-        )
 
     given Parser[Double] with
       override def parse(env: String): Either[Error, Double] =
-        Either.catchOnly[NumberFormatException](env.toDouble).leftMap(t =>
+        Either.catchOnly[NumberFormatException](env.toDouble).leftMap: t =>
           Error.ParsingError(s"failed to parse $env to `Double`")
-        )
 
     given Parser[FiniteDuration] with
       override def parse(env: String): Either[Error, FiniteDuration] =
         Either.catchOnly[IllegalArgumentException](Duration(env))
-          .leftMap(e => Error.ParsingError(e.getMessage))
-          .flatMap {
+          .leftMap: e =>
+            Error.ParsingError(e.getMessage)
+          .flatMap:
             case d: FiniteDuration => d.asRight[Error]
             case _                 => Left(Error.ParsingError(s"failed to parse $env to `FiniteDuration`"))
-          }
 
   def default[F[_]: Env: MonadThrow]: EnvProvider[F] =
     new EnvProvider[F]:
-      override def get[V](name: String)(using p: Parser[V]): F[Either[Error, V]] =
-        Env[F].get(name).map {
-          case Some(str) => p.parse(str)
+      override def get[V: Parser](name: String): F[Either[Error, V]] =
+        Env[F].get(name).map:
+          case Some(str) => Parser[V].parse(str)
           case None      => Error.MissingEnv(s"missing `${name}` env var").asLeft[V]
-        }
 
   def resource[F[_]: Env: MonadThrow]: Resource[F, EnvProvider[F]] =
     Resource.pure(this.default[F])
